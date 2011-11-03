eeb2a560b12f7a729986b1f58e5ca36028cb3eeb
Nick Partridge <nkpart@gmail.com>
2011-11-04 09:46:21 +1000

    Maybe

---
+++ lib/data/maybe.rb
@@ -0,0 +1,33 @@
+require 'adt'
+
+class Maybe
+  extend ADT
+  cases do
+    nothing
+    just(:value)
+  end
+
+  def map
+    fold(proc { self }, proc { |v| yield v })
+  end
+
+  def filter
+    fold(proc { self }, proc { |v| yield(v) ? self : Maybe.nothing })
+  end
+
+  def or_else(other = nil)
+    fold(proc { other || yield }, proc { |v| self }) 
+  end
+
+  def or_value(v = nil)
+    fold(proc { v || yield }, proc { |v| v })
+  end
+
+  def or_nil
+    fold(proc { nil }, proc { |v| v })
+  end
+
+  def self.from_nil(v)
+    v.nil? ? nothing : just(v)
+  end
+end
---
+++ spec/data/maybe_spec.rb
@@ -0,0 +1,43 @@
+require 'data/maybe'
+
+describe Maybe do
+  let(:one) { rand }
+  let(:two) { rand }
+  it("#map") { Maybe.just(5).map(&:succ).should == 6 }
+  it("#or_else") {
+    Maybe.just(one).or_else(Maybe.just(two)).should == Maybe.just(one)
+    Maybe.just(one).or_else{Maybe.just(two)}.should == Maybe.just(one)
+    Maybe.nothing.or_else(Maybe.just(two)).should == Maybe.just(two)
+  }
+
+  it("#or_value") {
+    Maybe.just(one).or_value(two).should == one
+    Maybe.nothing.or_value(two).should == two
+    Maybe.nothing.or_value { two }.should == two
+  }
+
+  it("#filter") {
+    Maybe.nothing.filter { |x| true }.should == Maybe.nothing
+    Maybe.just(one).filter { |x| x > one }.should == Maybe.nothing
+    Maybe.just(one).filter { |x| x == one }.should == Maybe.just(one)
+  }
+  
+  it("#to_a") {
+    Maybe.just(one).to_a.should == [one]
+    Maybe.nothing.to_a.should == []
+  }
+
+  it("::from_nil") {
+    Maybe.from_nil(nil).should == Maybe.nothing
+    Maybe.from_nil(one).should == Maybe.just(one)
+  }
+
+  # We get these for free from ADT
+  let(:just_and_nothing) { [Maybe.just(one), Maybe.nothing] }
+  it {
+    just_and_nothing.map(&:just?).should == [true, false]
+    just_and_nothing.map(&:nothing?).should == [false, true]
+    just_and_nothing.map(&:to_a).should == [[one], []]
+  }
+end
+
